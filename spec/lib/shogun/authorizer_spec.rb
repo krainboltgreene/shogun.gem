6a1020bc2e547bd5d5199a7ca1fabc817b7f26c9
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2014-10-31 07:32:12 -0500

    Implementing the first tests.

--- spec/lib/shogun/authorizer_spec.rb
+++ spec/lib/shogun/authorizer_spec.rb
@@ -1,5 +1,28 @@
 require "spec_helper"
 
 describe Shogun::Authorizer do
+  let(:authorizer_klass) do
+    class ExampleAuthorizer
+      include Shogun::Authorizer
+    end
+  end
+  let(:authorizer) { authorizer_klass.new }
 
+  describe "#denied?" do
+    let(:denied?) { authorizer.denied? }
+
+    context "if #allowed? is true" do
+      it "returns false" do
+        allow(authorizer).to receive(:allowed?).and_return(true)
+        expect(denied?).to be(false)
+      end
+    end
+
+    context "if #allowed? is false" do
+      it "returns true" do
+        allow(authorizer).to receive(:allowed?).and_return(false)
+        expect(denied?).to be(true)
+      end
+    end
+  end
 end
--- spec/lib/shogun/control_spec.rb
+++ spec/lib/shogun/control_spec.rb
@@ -1,5 +1,35 @@
 require "spec_helper"
 
 describe Shogun::Control do
+  let(:control_klass) do
+    class ExampleControl
+      include Shogun::Control
+    end
+  end
+  let(:control) { control_klass.new }
+  let(:status) { 200 }
+  let(:headers) { { a: :b } }
+  let(:body) { "foo" }
 
+  before(:each) do
+    allow(control).to receive(:status).and_return(status)
+    allow(control).to receive(:headers).and_return(headers)
+    allow(control).to receive(:body).and_return(body)
+  end
+
+  describe "#to_a" do
+    let(:to_a) { control.to_a }
+
+    it "returns an array with the status in first position" do
+      expect(to_a[0]).to be(status)
+    end
+
+    it "returns an array with the headers in second position" do
+      expect(to_a[1]).to be(headers)
+    end
+
+    it "returns an array with the body in third position" do
+      expect(to_a[2]).to be(body)
+    end
+  end
 end
