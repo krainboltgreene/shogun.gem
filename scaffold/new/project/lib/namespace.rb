5a55497efddef8c9d99b63c4d52ee6877b1dbb19
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-06-22 11:44:29 -0500

    Switching to ROM store mechanics

--- scaffold/new/project/lib/namespace.rb
+++ scaffold/new/project/lib/namespace.rb
@@ -14,9 +14,12 @@ Dotenv.load
 ENVied.require
 
 module {{namespace_module}}
-  require_relative "{{namespace}}/errors"
-  require_relative "{{namespace}}/accounts"
-  require_relative "{{namespace}}/sessions"
+  ROM.tap do |rom|
+    rom.setup(:memory)
+    require_relative "{{namespace}}/errors"
+    require_relative "{{namespace}}/accounts"
+    require_relative "{{namespace}}/sessions"
+  end.finalize
 
   def self.logger(io: STDOUT, level: ::Logger.const_get(ENV["APPLICATION_LOG_LEVEL"]))
     @logger ||= ::Logger.new(STDOUT).tap do |log|
