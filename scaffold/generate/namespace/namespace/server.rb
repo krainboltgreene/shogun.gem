1d5f3bad245d9e3639ae6abbae7c256755ef5683
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-08-21 10:48:01 -0500

    Rewriting to be more conscise

--- scaffold/generate/namespace/namespace/server.rb
+++ scaffold/generate/namespace/namespace/server.rb
@@ -3,23 +3,6 @@ module {{project_as_module}}
     module Server
       VERSION = "1.0.0"
 
-      ORIGIN_WHITELIST = Hamster::Vector.new([
-        ENV["CLIENT_HOST"]
-      ])
-
-      DESERIALIZATION_MAP = Hamster::Hash.new({
-        "*/*" => Oj,
-        "application/vnd.{{namespace}}+json; version=#{VERSION}" => Oj,
-        "application/json" => Oj
-      })
-
-      SERIALIZATION_MAP = Hamster::Hash.new({
-        "application/vnd.{{namespace}}+json; version=#{VERSION}" => Oj
-      })
-
-      DEFAULT_SERIALIZER = Oj
-
-
       def initialize(rack:, logger:)
         ActiveSupport::Cache::Store.logger = logger
 
@@ -30,22 +13,27 @@ module {{project_as_module}}
         ROM.finalize
 
         rack.use(Rack::Log, logger)
+
         rack.use(Rack::Runtime)
-        rack.use(Rack::Protection::HttpOrigin, origin_whitelist: ORIGIN_WHITELIST)
+
+        rack.use(Rack::Protection::HttpOrigin, origin_whitelist: [ENV["CLIENT_HOST"]])
+
         rack.use(Rack::Protection::EscapedParams)
+
         rack.use(Rack::Protection::XSSHeader)
+
         rack.use(Rack::Protection::FrameOptions)
+
         rack.use(Rack::Protection::PathTraversal)
+
         rack.use(Rack::Protection::IPSpoofing)
+
         rack.use(Rack::Chunker)
 
-
-
-        ACCEPT_TYPES = Hamster::Vector.new([
+        rack.use(Rack::AcceptSetter, [
           "application/vnd.{{namespace}}+json; version=#{VERSION}",
           "application/json"
         ])
-        rack.use(Rack::AcceptSetter, ACCEPT_TYPES)
 
         rack.use(Rack::ContentLengthSetter)
 
@@ -57,15 +45,22 @@ module {{project_as_module}}
           end
         end
 
-        rack.use(Rack::BodyDeserializer, DESERIALIZATION_MAP)
-        rack.use(Rack::BodySerializer, SERIALIZATION_MAP, DEFAULT_SERIALIZER)
-
         rack.use(Rack::Cors) do |policy|
           policy.allow do
             origins(ENV["SERVER_HOST"], ENV["CLIENT_HOST"])
             resource("*", headers: :any, methods: [:get, :post, :patch, :post, :delete, :options, :head])
           end
         end
+
+        rack.use(Rack::BodyDeserializer, {
+          "*/*" => Oj,
+          "application/vnd.{{namespace}}+json; version=#{VERSION}" => Oj,
+          "application/json" => Oj
+        })
+
+        rack.use(Rack::BodySerializer, {
+          "application/vnd.{{namespace}}+json; version=#{VERSION}" => Oj
+        }, Oj)
 
         rack.run(Shogun::Dispatch.new(logger: logger) do |router|
           Accounts::Endpoint.new(router: router)
